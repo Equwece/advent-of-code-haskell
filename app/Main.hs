43746a210d3d5006adfd9fb83f423cebbaefa4c1
Equwece <99554597+Equwece@users.noreply.github.com>
2023-01-22 03:08:58 +0300

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Day1 (countTopThreeCalories)
+
+main :: IO ()
+main = countTopThreeCalories
---
+++ src/Day1.hs
@@ -0,0 +1,37 @@
+module Day1 (readStringToInt
+            ,countCalories
+            ,splitByNewLine
+            ,countTopThreeCalories
+)
+where
+import Data.List (sort)
+
+countCalories :: IO ()
+countCalories = do
+  parsedResult <- maximum . map (sum . map readStringToInt) . splitByNewLine . lines <$> getContents
+  print parsedResult
+
+readStringToInt :: String -> Int
+readStringToInt str = read str :: Int
+
+splitByNewLine :: (Foldable t) => [t a] -> [[t a]]
+splitByNewLine [] = []
+splitByNewLine lns = newPart : splitByNewLine reminder
+  where
+    getNewLineIndex [] i = i
+    getNewLineIndex (x:xs) i
+      | null x = i
+      | otherwise = getNewLineIndex xs (i + 1)
+    newLineIndex = getNewLineIndex lns 0
+    newPart = take newLineIndex lns
+    reminder = drop (newLineIndex + 1) lns
+
+----------------------------------------
+-- Task 2
+----------------------------------------
+
+countTopThreeCalories :: IO ()
+countTopThreeCalories = do
+  parsedResult <- sum . take 3 . reverse . sort . map (sum . map readStringToInt) . splitByNewLine . lines <$> getContents
+  print parsedResult
+
